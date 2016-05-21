f76e97bd6547d295cd96d06b277726e850e68930
Rodrigo Dias <rodrigo@rodderscode.co.uk>
2016-05-21 10:34:16 +0100

    latest stuff with id

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var Dim = {
 }
 Dim.game = {x:0, y:0, w:Dim.stage.x, h:(Dim.stage.y/12)*7};
 Dim.menu = {x:-1, y:Dim.game.h, w:Dim.stage.x, h:Dim.stage.y};
+Dim.cube = {w:20, h:20}
+
 
 var total_cubes = 558;
 // var screen = {x: world_geom.maxX, y: world_geom.maxY};
@@ -26,55 +28,28 @@ function create(){
   menu = game.add.image(Dim.menu.x, Dim.menu.y, 'menu');
   menu.width = menu.width + 1; // some problem with the menu missing a pixel... :(
   createCubes();
-  createRoad();
-  //game.input.onDown.add(unpause, self);
+  // createRoad();
+  //game.input.onDown.add(click, self);
 };
 
 function update(){}
 
 function render() {}
 
-function unpause(event){
-        // Only act if paused
-        if(game.paused){
-            // Calculate the corners of the menu
-            var x1 = w/2 - 270/2, x2 = w/2 + 270/2,
-                y1 = h/2 - 180/2, y2 = h/2 + 180/2;
-
-            if(event.x > x1 && event.x < x2 && event.y > y1 && event.y < y2 ){
-                var choisemap = ['one', 'two', 'three', 'four', 'five', 'six'];
-
-                // Get menu local coordinates for the click
-                var x = event.x - x1,
-                    y = event.y - y1;
-
-                // Calculate the choice
-                var choise = Math.floor(x / 90) + 3*Math.floor(y / 90);
-                //choiseLabel.text = 'You chose menu item: ' + choisemap[choise];
-            }
-            else{
-                menu.destroy();
-                choiseLabel.destroy();
-                game.paused = false;
-            }
-        }
-    };
-
-
 var createRoad = function(){
   // cube at the top to start a road at.
   // Select one of the first cubes at x
-  var x = Math.round(game.rnd.between(0, Dim.game.w)/20);
-  console.log(x);
+  var x = Math.round(game.rnd.between(0, Dim.game.w)/Dim.cube.w);
   var y = 0;
   while(y <= Dim.game.h){
-    var cube = getCube(x+y);
+    var cube = getCube([x,y]);
     cube.sprite.destroy();
     y++;
   }
 }
 
-
+var max_hor_cubes = Dim.game.w/Dim.cube.w;
+var max_vert_cubes;
 var getCube = function(key){
   for(i in cubes){
     if (cubes[i].key == key)
@@ -82,15 +57,49 @@ var getCube = function(key){
   }
 }
 
+
 var createCubes = function(){
   var x = Dim.game.x;
   var y = Dim.game.y;
+  var depth = 0;
   for (var i = 0; i < total_cubes-1; i++) {
     x += 20;
     if (x > Dim.game.w){
       x = 0;
       y += 20;
+      depth++;
     }
-    cubes.push(new Cube(x,y));
+    var id = [depth, x/Dim.cube.w];
+    var c = new Cube(x,y,id);
+    cubes.push(c);
   }
+  max_vert_cubes = depth;
 }
+
+
+
+function click(event){
+    // Only act if paused
+    if(game.paused){
+        // Calculate the corners of the menu
+        var x1 = w/2 - 270/2, x2 = w/2 + 270/2,
+            y1 = h/2 - 180/2, y2 = h/2 + 180/2;
+
+        if(event.x > x1 && event.x < x2 && event.y > y1 && event.y < y2 ){
+            var choisemap = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+            // Get menu local coordinates for the click
+            var x = event.x - x1,
+                y = event.y - y1;
+
+            // Calculate the choice
+            var choise = Math.floor(x / 90) + 3*Math.floor(y / 90);
+            //choiseLabel.text = 'You chose menu item: ' + choisemap[choise];
+        }
+        else{
+            menu.destroy();
+            choiseLabel.destroy();
+            game.paused = false;
+        }
+    }
+};
--- models/Cube.js
+++ models/Cube.js
@@ -1,8 +1,9 @@
 var Cube = function(x,y,i){
   var x = x;
   var y = y;
-  var w = 10 ,h = 10;
+  
   this.sprite = game.add.sprite(x, y, 'cube');
-	this.sprite.anchor.set(1);
+  this.sprite.anchor.set(1);
   this.key = i;
+
 }
